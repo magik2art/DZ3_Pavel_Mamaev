85b702cf849f86eb04f5af8bde68a6782b38e7e9
Poul <magik2art@gmail.com>
2021-04-20 15:53:45 +0300

    plus second task

--- 1. and 2.Function num_translate.py
+++ 1. and 2.Function num_translate.py
@@ -1,11 +1,11 @@
 glossary_num = {
     1: "one", 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-    6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten'
+    6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 0:'zero'
 }
 
 glossary_str = {
     'один': 'one', 'два': 'two', 'три': 'three', 'четыре': 'four', 'пять': 'five',
-    'шесть': 'six', 'семь': 'seven', 'восемь': 'eight', 'девять': 'nine', 'десять': 'ten'
+    'шесть': 'six', 'семь': 'seven', 'восемь': 'eight', 'девять': 'nine', 'десять': 'ten', 'ноль': 'zero'
 }
 
 def func_translate(num_for_translate):
@@ -14,10 +14,18 @@ def func_translate(num_for_translate):
     else:
         print(None)
 
+def func_str_capitalize(num_for_translate):
+    num_for_translate = num_for_translate.lower()
+    if num_for_translate in glossary_str:
+        num_for_translate = glossary_str[num_for_translate]
+        print(num_for_translate.capitalize())
+    else:
+        print(None)
 
 def func_str_translate(num_for_translate):
     if num_for_translate in glossary_str:
-        print(glossary_str[num_for_translate])
+        num_for_translate = glossary_str[num_for_translate]
+        print(num_for_translate)
     else:
         print(None)
 
@@ -25,6 +33,7 @@ num_for_translate = input("Введите число от 0 до 10: ")
 if num_for_translate.isdigit():
     num_for_translate = int(num_for_translate)
     func_translate(num_for_translate)
+elif num_for_translate[0].isupper():
+    func_str_capitalize(num_for_translate)
 else:
-    num_for_translate = num_for_translate.lower()
     func_str_translate(num_for_translate)
